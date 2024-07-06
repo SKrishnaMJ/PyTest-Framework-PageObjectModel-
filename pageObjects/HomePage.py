fbc0b3507c1e141dee8432712ee3a2187eb1c992
SKrishnaMJ <146481712+SKrishnaMJ@users.noreply.github.com>
2024-07-06 10:27:09 -0500

    created another script that is optimised and automates a form submission

--- pageObjects/HomePage.py
+++ pageObjects/HomePage.py
@@ -9,6 +9,14 @@ class HomePage:
         self.driver = driver
 
     shop = (By.LINK_TEXT,"Shop")
+    name = (By.NAME, "name")
+    email = (By.NAME, "email")
+    password = (By.ID, "exampleInputPassword1")
+    checkbox = (By.ID, "exampleCheck1")
+    radioBtn = (By.ID, "inlineRadio2")
+    submit = (By.XPATH, "//input[@type='submit']")
+    succesMsg = (By.CLASS_NAME, "alert-success")
+    dropdown = (By.ID, "exampleFormControlSelect1")
 
     def gotoShop(self):
         self.driver.find_element(*HomePage.shop).click()
@@ -16,3 +24,29 @@ class HomePage:
         # we perform the click operation here and then create the shopPage object here and return the shopPage to optimise our testcode
         shopPage = ShopPage(self.driver)
         return shopPage
+
+    def getName(self):
+        return self.driver.find_element(*HomePage.name)
+
+    def getEmail(self):
+        return self.driver.find_element(*HomePage.email)
+
+    def getPassword(self):
+        return self.driver.find_element(*HomePage.password)
+
+    def getCheckbox(self):
+        return self.driver.find_element(*HomePage.checkbox)
+
+    def getRadioBtn(self):
+        return self.driver.find_element(*HomePage.radioBtn)
+
+    def getSubmitBtn(self):
+        return self.driver.find_element(*HomePage.submit)
+
+    def getMessage(self):
+        return self.driver.find_element(*HomePage.succesMsg)
+
+    def getDropdown(self):
+        return self.driver.find_element(*HomePage.dropdown)
+
+
---
+++ tests/test_HomePage.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.support.select import Select
+
+from pageObjects.HomePage import HomePage
+from utilities.BaseClass import BaseClass
+
+
+class TestHomepage(BaseClass):
+    def test_FormSubmission(self):
+        homePage = HomePage(self.driver)
+        homePage.getName().send_keys("Sai Krishna M J")
+        homePage.getEmail().send_keys("saik@gmail.com")
+        homePage.getPassword().send_keys("12345")
+        homePage.getCheckbox().click()
+
+        # Select class to handle static dropdown
+        dropdown = Select(homePage.getDropdown())
+        dropdown.select_by_visible_text("Female")
+
+        homePage.getRadioBtn().click()
+
+        # Xpath Syntax - //tagname[@attribute_name='value'] --> //input[@type='submit']
+        homePage.getSubmitBtn().click()
+        message = homePage.getMessage().text
+        print(message)
+
+        # to pass or fail tests we use assert class
+        assert "Success" in message
