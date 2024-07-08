e96b967064bdd1f0ee836534a80734bc77c9ab7a
SKrishnaMJ <146481712+SKrishnaMJ@users.noreply.github.com>
2024-07-08 08:41:33 -0500

    added a utility in conftest.py that captures a screenshot on test failure and places it in the report.html

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,9 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
+# We define driver here so that ot can be used globally throughout this class across all methods later
+driver = None
+
 
 # This function helps set command line variables, here we have defined browser_name
 def pytest_addoption(parser):
@@ -17,6 +20,8 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="class")
 def setup(request):
+    # This global keyword allows the driver to be used anywhere within this class
+    global driver
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
         chromeOptions = webdriver.ChromeOptions()
@@ -35,3 +40,30 @@ def setup(request):
     request.cls.driver = driver
     yield
     driver.close()
+
+# This method gets called whenever there is a failure in a test and attaches the screenshot of the step failed to the report.html
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+        driver.get_screenshot_as_file(name)
